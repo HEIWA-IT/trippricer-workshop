6226ae5dc76486263f0455287575224786224146
Dan <dan@heiwa-it.com>
2022-04-13 07:31:18 +0200

    [FE] The annotation cucumber is deprecated. Replacing it with @Suite of Junit 5.

--- exposition/src/test/java/features/trip/CucumberTest.java
+++ exposition/src/test/java/features/trip/CucumberTest.java
@@ -1,7 +1,15 @@
 package features.trip;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:build/cucumber/TripAgency.json, html:build/cucumber/TripAgency.html, pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "features.trip")
 public class CucumberTest {
 }
